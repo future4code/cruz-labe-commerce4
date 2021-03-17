9651f317e7d63c95b67819b7dc62eedda6710597
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-03-17 15:21:24 -0300

    adicionado estrutura da lista de arquivos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import ListaProdutos from './ListaProdutos'
 
 class App extends Component {
+  state = {
+    filtros: {
+      valorMin: 0,
+      valorMax: 0,
+      ordem: true,
+      inputPesquisar: ''
+    },
+    carrinho: [1, 3]
+
+  }
   render () {
+    //const produtos = ListaProdutos.filter(produto =>)
+    //
     return (
       <div className="App">
         <h1>Lab Commerce</h1>
