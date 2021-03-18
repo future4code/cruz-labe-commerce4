b732ddec489161cebba0a6c54224e2d100d6259a
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-03-18 16:31:14 -0300

    update

--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import React, { Component } from 'react'
+import { Button } from '@material-ui/core'
     
 export class Filtro extends Component {
   render() {
@@ -7,15 +8,26 @@ export class Filtro extends Component {
       <Container>
           <Title>Filtrar Pesquisas</Title>
           <h2>Preço</h2>
-          <input onChange={this.props.atualizar} type="range" name="valorMin" id="min" min="0" max="100"/>
-          <label htmlFor="min">Minimo</label>
-          <input onChange={this.props.atualizar} type="range" name="valorMax" id="max" min="101" max="1000"/>
-          <label htmlFor="min">Máximo</label>
+          <input onChange={this.props.atualizar} type="range" name="valorMin"
+          value={this.props.valorMin} id="min" min="0" max="900" step='100'/>
+          <label htmlFor="min">Minimo: {this.props.valorMin}</label>
+          <input onChange={this.props.atualizar} type="range" name="valorMax"
+          value={this.props.valorMax} id="max" min="100" max="1000" step='100'/>
+          <label htmlFor="max">Máximo: {this.props.valorMax}</label>
           <h2>Ordem</h2>
-          <select onChange={this.props.atualizar} name="ordem">
+          <select onChange={this.props.atualizar} value={this.props.ordem}name="ordem">
+          <option value="recomendados">Recomendados</option>
           <option value="crescente">Crescente</option>
           <option value="decrescente">Decrescente</option>
           </select>
+          <p></p>
+          <Button 
+          onClick={this.props.limpa}
+          variant='contained'
+          color='primary'
+          >
+            Limpar Filtros
+            </Button>
       </Container>
     )
   }
