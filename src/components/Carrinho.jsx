caadf16f996b91b91a642a41bea6d1d363870aa6
Emilly-sant <emillysantiago89@gmail.com>
2021-03-19 15:46:03 -0300

    first commit

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
+import { Button } from '@material-ui/core'
 import { CarrinhoItem } from './CarrinhoItem'
 
-export class Carrinho extends Component{
-  
+export class Carrinho extends Component {
+
   //se vier produtos: [1, 1, 2]
-  
+
 
 
   render() {
@@ -14,25 +14,28 @@ export class Carrinho extends Component{
     const produtos = this.props.produtos.map(produto => {
       return (
         <CarrinhoItem
-        key={produto.id}
-        id={produto.id}
-        nome={produto.nome}
-        quantidade={produto.quantidade}
-        valor={produto.valor}
-        excluir={this.props.excluir}
+          key={produto.id}
+          id={produto.id}
+          nome={produto.nome}
+          quantidade={produto.quantidade}
+          valor={produto.valor}
+          excluir={this.props.excluir}
         />
       )
     })
 
-    const total = this.props.produtos.reduce((soma, produto) => 
+    const total = this.props.produtos.reduce((soma, produto) =>
       soma + produto.valor * produto.quantidade, 0)
 
-    return(
-        <Container>
-          {produtos}
-          {total}
-          <button onClick={this.props.limpar}>Limpar Tudo</button>
-        </Container>
+    return (
+      <Container>
+        {produtos}
+        {total}
+        <Button variant='contained'
+          color='primary'
+          onClick={this.props.limpar}>Limpar Tudo
+          </Button>
+      </Container>
     )
   }
 }
